237562600d3764ef851e669fd97a7c5ae88cc61e
Rolodex <rolodex888@gmail.com>
2016-02-19 00:21:05 +0800

    Added some comments

--- www/app/stayGanu.config.js
+++ www/app/stayGanu.config.js
@@ -9,7 +9,9 @@
     function StartUp($ionicPlatform) {
         $ionicPlatform.ready(function() {
             if (window.cordova && window.cordova.plugins.Keyboard) {
+                // Hide the accessory bar by default
                 cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                // Stops the viewport from snapping when text inputs are focused.
                 cordova.plugins.Keyboard.disableScroll(true);
             }
             if (window.StatusBar) {
